21b1019f101fe4f694a1e65817bf9cd70e119fe9
fahad <mdf780709@gmail.com>
2026-06-16 18:38:59 +0600

    add all revision

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,12 @@ import AboutSection from "@/components/home/About";
 import ClientMarquee from "@/components/home/ClientMarquee";
 import ContactSection from "@/components/home/ContactSection";
 import HeroSection from "@/components/home/Hero";
+import ProcessSection from "@/components/home/ProcessSection";
 import RecentPostsSection from "@/components/home/RecentPost";
 import ServicesGrid from "@/components/home/ServicesGrid";
 import SocialGallery from "@/components/home/SocialGallery";
 import StatsCounter from "@/components/home/StatsCounter";
+import TestimonialSection from "@/components/home/TestimonialSection";
 import VideoGallery from "@/components/home/VideoGallery";
 import Image from "next/image";
 
@@ -17,6 +19,8 @@ export default function Home() {
       <VideoGallery></VideoGallery>
       <StatsCounter></StatsCounter>
       <ServicesGrid></ServicesGrid>
+      <TestimonialSection></TestimonialSection>
+      <ProcessSection></ProcessSection>
       <ClientMarquee></ClientMarquee>
       <RecentPostsSection></RecentPostsSection>
       <SocialGallery></SocialGallery>
